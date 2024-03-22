c79c3951f82a0f944cea4492e7c8159c1f0c5bec
Tejalaps <tejalaps@gmail.com>
2024-03-22 16:23:43 +0530

    Update Text conversion.py

--- Text conversion.py
+++ Text conversion.py
@@ -13,20 +13,33 @@ for filename in os.listdir(directory):
         
         with open(os.path.join(directory, filename),'r') as src_file:
             text_data = src_file.read()
-        name_matches = re.findall(r'Name:\s(.*?)\s',text_data)
-        father_matches = re.findall(r"Father's Name\s:\s(.*?)\s",text_data)
+        name_matches = re.findall(r'Name:\s([A-Z\s]+)\s', text_data)
+        father_matches = re.findall(r"(Father|Husband|Mother)'s Name\s:\s([A-Z\s]+)\s",text_data)
         
-        names = []
-        fathers = []
-        for i in range(len(name_matches)):
-            names.append(name_matches[i])
-            if i<len(father_matches):
-                fathers.append(father_matches[i])
+#         names = []
+#         fathers = []
+#         for i in range(len(name_matches)):
+#             names.append(name_matches[i])
+#             if i<len(father_matches):
+#                 fathers.append(father_matches[i])
         
-        for i, name in enumerate(names):
+        for i, name in enumerate(name_matches):
+            name_parts = name.strip().split()
+            if len(name_parts)>=2:
+                full_name = ' '.join(name_parts)
+            else:
+                full_name = name.strip()
+#             Relationship
+            father_name = father_matches[i][0].strip()   
+            father_parts = father_name.split()
+            if len(father_parts)>=2:
+                full_father_name = ' '.join(father_parts[:2])
+            else:
+                full_father_name = father_name
+       
             row= {
-                'Name': name.strip(),
-                "Father\'s Name": fathers[i].strip() if i<len(fathers) else ''
+                'Name': full_name,
+                "Relationship": full_father_name
             }
             rows.append(row)
 df = pd.DataFrame(rows)
